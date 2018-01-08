1e97d7cf1b5b01907bd8ca3f5cf5fdfd8c962521
zifengb <987400803@qq.com>
2018-01-08 21:41:26 +0800

    单个订单页

--- src/vue/pages/order-single.vue
+++ src/vue/pages/order-single.vue
@@ -69,6 +69,20 @@
 		margin: 0;
 	}
 }
+
+.inner-grid {
+	width: 100%;
+	div[class*='col'] {
+		margin: 2% 0;
+	}
+	.badge {
+		margin-right: 2%;
+		background-color: #A938A9;
+		border-radius: 0;
+		vertical-align: text-bottom;
+	}
+}
+
 // 自定义picker-modal
 .modal-popup {
 	background-color: rgba(0, 0, 0, 0.4);
@@ -103,7 +117,7 @@
 		</div>
 
 		<f7-tabs>
-			<f7-tab id="tab-1">
+			<f7-tab id="tab-1" active>
 				<f7-timeline class="timeline-list">
 					<f7-timeline-item class="timeline-list-item">
 						<f7-grid class="tl-item-content">
@@ -152,41 +166,129 @@
 					</f7-timeline-item>
 				</f7-timeline>
 			</f7-tab>
-			<f7-tab id="tab-2" active>
+			<f7-tab id="tab-2">
 
 				<!-- 订单详情 start -->
 				<f7-block-title class="block-title">订单详情</f7-block-title>
 				<f7-list class="order-detail">
-					<f7-list-item class="restaurant">hkhk</f7-list-item>
-					<f7-list-item class="food-list">hkhk</f7-list-item>
-					<f7-list-item class="fee-list">hkhk</f7-list-item>
-					<f7-list-item class="coupun-list">hkhk</f7-list-item>
-					<f7-list-item class="sum">hkhk</f7-list-item>
+					<f7-list-item class="restaurant">
+						<f7-grid class="inner-grid">
+							<f7-col width="70">墨刀餐厅</f7-col>
+							<f7-col width="30">非美团转送</f7-col>
+						</f7-grid>
+					</f7-list-item>
+					<f7-list-item class="food-list">
+						<f7-grid class="inner-grid">
+							<!-- item -->
+							<f7-col width="55">
+								<f7-badge>折</f7-badge>
+								菜式1
+							</f7-col>
+							<f7-col width="15">* 1</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<!-- item -->
+							<!-- item -->
+							<f7-col width="55">
+								<f7-badge>折</f7-badge>
+								菜式1
+							</f7-col>
+							<f7-col width="15">* 1</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<!-- item -->
+							<!-- item -->
+							<f7-col width="55">
+								<f7-badge>折</f7-badge>
+								菜式1
+							</f7-col>
+							<f7-col width="15">* 1</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<!-- item -->
+						</f7-grid>
+					</f7-list-item>
+					<f7-list-item class="fee-list">
+						<f7-grid class="inner-grid">
+							<f7-col width="85">餐盒费</f7-col>
+							<f7-col width="15">￥10</f7-col>
+							<f7-col width="85">配送费</f7-col>
+							<f7-col width="15">￥10</f7-col>
+						</f7-grid>
+					</f7-list-item>
+					<f7-list-item class="coupun-list">
+						<f7-grid class="inner-grid">
+							<f7-col width="85">
+								<f7-badge>折</f7-badge>
+								门店新客
+							</f7-col>
+							<f7-col width="15">
+								-￥5
+							</f7-col>
+							<f7-col width="85">
+								<f7-badge>返</f7-badge>
+								满返商家代金券优惠
+							</f7-col>
+							<f7-col width="15">
+								-￥5
+							</f7-col>
+						</f7-grid>
+					</f7-list-item>
+					<f7-list-item class="final-fee">
+						<f7-grid class="inner-grid">
+							<f7-col width="70">
+								<span>总计 ￥10</span>
+								<span>优惠 ￥10</span>
+							</f7-col>
+							<f7-col width="30">实付 ￥10</f7-col>
+						</f7-grid>
+					</f7-list-item>
 				</f7-list>
 				<!-- 订单详情 end -->
 
 				<!-- 配送 start -->
 				<f7-block-title class="block-title">配送信息</f7-block-title>
 				<f7-list class="delivery-info">
-					<f7-list-item class="delivery-tip">hkhk</f7-list-item>
-					<f7-list-item class="address">hkhk</f7-list-item>
-					<f7-list-item class="service">hkhk</f7-list-item>
+					<f7-list-item class="delivery-tip">
+						<f7-label>期望xxx</f7-label>
+						<span>立即配送</span>
+					</f7-list-item>
+					<f7-list-item class="address">
+						<f7-label>配送地址</f7-label>
+						<div>
+							<p>濛子（女士） 213131534</p>
+							<p>这是地址这是地址这是地址这是地址</p>
+						</div>
+					</f7-list-item>
+					<f7-list-item class="service">
+						<f7-label>配送服务</f7-label>
+						<span>由 商家 提供配送服务</span>
+					</f7-list-item>
 				</f7-list>
 				<!-- 配送 end -->
 
 				<!-- 订单信息 start -->
 				<f7-block-title class="block-title">订单信息</f7-block-title>
 				<f7-list class="order-info">
-					<f7-list-item class="phone">hkhk</f7-list-item>
-					<f7-list-item class="time">hkhk</f7-list-item>
-					<f7-list-item class="method">hkhk</f7-list-item>
+					<f7-list-item class="phone">
+						<f7-label>订单号码</f7-label>
+						<span>12312312</span>
+					</f7-list-item>
+					<f7-list-item class="time">
+						<f7-label>订单时间</f7-label>
+						<span>1231233</span>
+					</f7-list-item>
+					<f7-list-item class="method">
+						<f7-label>支付方式</f7-label>
+						<span>线上支付</span>
+					</f7-list-item>
 				</f7-list>
 				<!-- 订单信息 end -->
 
 			</f7-tab>
 		</f7-tabs>
 
-		<!-- <f7-actions :opened="flag" class="action-modal" @actions:closed="actionModal(false)">
+		<f7-actions :opened="flag" class="action-modal" @actions:closed="actionModal(false)">
 			<f7-actions-group>
 				<f7-actions-button>商家电话12312132</f7-actions-button>
 				<f7-actions-button>客服电话12312132</f7-actions-button>
@@ -194,9 +296,9 @@
 			<f7-actions-group>
 				<f7-actions-button>取消</f7-actions-button>
 			</f7-actions-group>
-		</f7-actions> -->
+		</f7-actions>
 
-		<f7-picker-modal class="modal-popup" :opened="flag">
+		<!-- <f7-picker-modal class="modal-popup" :opened="flag">
 			<div class="popup-content">
 				<p>fsdfd</p>
 				<p>fsdfd</p>
@@ -207,7 +309,7 @@
 				<p>fsdfd</p>
 				<f7-button @click="popup">关闭按钮</f7-button>
 			</div>
-		</f7-picker-modal>
+		</f7-picker-modal> -->
 
 	</f7-page>
 </template>
